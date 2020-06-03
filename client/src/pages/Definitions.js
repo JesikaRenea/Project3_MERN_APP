8721484831dd1d700e060bc50280bffb360f94eb
JesikaRenea <JessicaHayesUCF@outlook.com>
2020-06-03 14:59:25 -0400

    Working Library Display, Definitions Display, and Categories Table Display

--- client/src/pages/Definitions.js
+++ client/src/pages/Definitions.js
@@ -1,53 +1,121 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from "../components/layout/Navbar";
 import SkillsTable from "../components/layout/SkillsTable";
+import CategoriesTable from "../components/layout/CategoriesTable";
+
 import DefCard from "../components/cards/DefCard";
 import Main from "../components/Main";
 import API from "../utils/API"
 
 
-
-
 const Definitions = props => {
-    const [getSkills, setGetSkills] = useState(true)
-    const [skills, setSkills] = useState([])
-  
-    useEffect(() => {
-      {
-        if (getSkills)
-          API.getAllSkillsByUser()
-            .then(res => {
-              console.log(res.data);
-              setSkills(res.data.map(skill => ({
-                id: skill._id,
-                name: skill.skillsName
-              })
-              ))
-            }
-            )
-            .catch(
-              error => console.log(error.response.data)
-            )
-            
-        setGetSkills(false)}
-    },
-      [getSkills]
-    );
+  const [getSkills, setGetSkills] = useState(true)
+  const [skills, setSkills] = useState([]);
 
-    return (
-<div>
-    <Navbar />
-        <div className="row mainRow1">
-            <div className="col s2">
-                <SkillsTable skills={skills}/>
-            </div>
-                <div className="col s10">
-                    <h4 className="page-title">My Definitions</h4>
-                    <DefCard />
-                </div>
-            </div>
+  const [getDefs, setGetDefs] = useState([true]);
+  const [defs, setDefs] = useState([]);
+
+  const [getCategories, setGetCategories] = useState(true)
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    {
+      if (getCategories)
+        API.findAllCat()
+          .then(res => {
+            console.log(res.data);
+            setCategories(res.data.map(category => ({
+              id: category._id,
+              catName: category.catName
+            })
+            ))
+          }
+          )
+          .catch(
+            error => console.log(error.response.data)
+          )
+
+      setGetCategories(false)
+    }
+  },
+    [getCategories]
+  );
+
+  useEffect(() => {
+    {
+      if (getSkills)
+        API.getAllSkillsByUser()
+          .then(res => {
+            console.log(res.data);
+            setSkills(res.data.map(skill => ({
+              id: skill._id,
+              name: skill.skillsName
+            })
+            ))
+          }
+          )
+          .catch(
+            error => console.log(error.response.data)
+          )
+
+      setGetSkills(false)
+    }
+  },
+    [getSkills]
+  );
+
+  useEffect(() => {
+    {
+      if (getDefs)
+        API.getAllDefByUser()
+          .then(res => {
+            console.log(res.data);
+            setDefs(res.data.map(def => ({
+              id: def._id,
+              defName: def.defName,
+              defText: def.defText,
+              cat: def.Cat,
+            })
+            ))
+          }
+          )
+          .catch(
+            error => console.log(error.response.data)
+          )
+
+      setGetDefs(false)
+    }
+  },
+    [getDefs]
+  );
+
+
+  return (
+    <div>
+      <Navbar />
+      <div className="row mainRow1">
+        <div className="col s2">
+          <CategoriesTable categories={categories} />
+          <br />
+          <SkillsTable skills={skills} />
         </div>
-    );
+        <div className="col s10">
+          <h4 className="page-title">My Definitions</h4>
+
+          {defs.map(def => (
+            <DefCard
+              id={def.id}
+              defName={def.defName}
+              defText={def.defText}
+              cat={def.Cat}
+            />
+          )
+          )
+          }
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Definitions
